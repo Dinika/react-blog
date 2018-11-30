91bce6249c588f9d99069447bc22157fb3025d68
Dinika <dinikasaxenas@gmail.com>
2018-11-30 17:54:49 +0530

    guard NewPost
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,6 +6,9 @@ import NewPost from '../NewPost/NewPost';
 import FullPost from '../FullPost/FullPost';
 
 class Blog extends Component {
+    state = {
+        authenticated: false
+    }
     render() {
         return (
             <div>
@@ -18,7 +21,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    { this.state.authenticated ? <Route path="/new-post" component={NewPost} /> : null }
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
